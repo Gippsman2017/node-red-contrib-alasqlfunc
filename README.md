2bbe4131fc710fbcd71a5ff7f236f29aca5d6c72
gippsman2017 <warracoder@combatwombat.net>
2019-12-03 15:41:43 +1100

    1st release

---
+++ README.md
@@ -0,0 +1,21 @@
+# AlaSQLFunc additional Alasql nodenode
+
+## node-red-contrib-alasqlfunc
+
+This additional  Node-red `alasql` node lets you use msg.query as the query/insert/update
+
+## Eg: 
+
+msg.query='select * from abc where Id="'+msg.payload.response.id+'"';
+
+or
+
+var a = 'wombat';
+var b = 'platypus';
+msg.query='insert into abc values ("'+a+'","'+b,+'")';
+
+![xkcd](https://cloud.githubusercontent.com/assets/1063454/13614823/999e9548-e572-11e5-9661-57a06e8f3fa4.png)
+
+
+
+
---
+++ alasqlfunc.js
@@ -0,0 +1,26 @@
+module.exports = function (RED) {
+    var alasql = require('alasql');
+
+    function AlasqlFuncNodeIn(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.query = config.query;
+        node.on("input", function (msg) {
+            var sql = msg.query||node.query;
+            var bind = Array.isArray(msg.payload) ? msg.payload : [msg.payload];
+            alasql.promise(sql, [bind])
+                .then(function (res) {
+                    msg.payload = res;
+                    node.status({fill: "green", shape: "dot", text: ' Records: ' + msg.payload.length});
+                    node.send(msg);
+                }).catch((err) => {
+                node.error(err, msg);
+            });
+        });
+        this.on('close', () => {
+            node.status({});
+        });
+    }
+
+    RED.nodes.registerType("alasqlfunc", AlasqlFuncNodeIn);
+};
